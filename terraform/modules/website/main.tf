903a485f68266afe365003c8bc2b43ccce78aa0c
Dylan Vaughn <dylancvaughn@gmail.com>
2017-12-14 23:07:42 -0800

    add in CloudFront invalidation

--- terraform/modules/website/main.tf
+++ terraform/modules/website/main.tf
@@ -22,9 +22,11 @@ resource "aws_cloudformation_stack" "pipeline_bucket" {
 
 resource "aws_cloudformation_stack" "website_cicd" {
   capabilities = ["CAPABILITY_IAM"]
+  depends_on = ["aws_cloudformation_stack.website_bucket_and_cf"]
   name = "${var.env}-website-cicd-stack"
   on_failure = "DELETE"
   parameters {
+    CloudFrontDistributionId = "${aws_cloudformation_stack.website_bucket_and_cf.outputs["CloudFrontDistributionId"]}"
     PipelineBucket = "${aws_cloudformation_stack.pipeline_bucket.outputs["PipelineBucket"]}"
     SourceCodeCommitRepoBranch = "${var.code_commit_repo_branch}"
     SourceCodeCommitRepoName = "${var.code_commit_repo_name}"
